0cbb6f58cae82a4c24a0a1579893cc8ee4eff59b
Víz Elek <viz@elek.com>
2022-10-18 10:03:46 +0200

    Added calculator.py
    TODO: implement functions

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# create functions for different operations
+
+def addition():
+    pass
+
+def subtraction():
+    pass
+
+def multiplication():
+    pass
+
+def division():
+    pass
+
+def pow():
+    pass
+
+def modulo():
+    pass
+
+def absolute():
+    pass
+
+def maxnumber():
+    pass
+
+def minnumber():
+    pass
